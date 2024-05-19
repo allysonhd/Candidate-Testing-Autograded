4b94af22b66d4f8a99145184f15f9d487e9f7bc0
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-05-19 12:31:28 -0500

    Part 2 graded assignment complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,17 +6,15 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-question = "Who was the first American woman in space? ";
-correctAnswer = "Sally Ride";
-candidateAnswer = "";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", 3];
+let candidateAnswers = [];
 
 
 function askForName()
@@ -27,18 +25,27 @@ candidateName = input.question("Enter your name: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log(question);
-candidateAnswer = input.question("Enter your answer: ");
-}
+for (index = 0; index < questions.length; index++) {
+    console.log(questions[index]);
+
+    const candidateResponse = input.question("Enter your answer: ");
+    candidateAnswers.push(candidateResponse);
+    
+    }
+};
+//console.log(candidateAnswers);
+//ask each question and get a response before moving on to the next question.
+//start at the first question - ask it, get a response, store the response, Ask the next question
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-   console.log("Correct! Great work!")
-} else {
-  console.log("Nice try, but that is incorrect.");
-}
+
+  console.log(`Your answers: ${candidateAnswers.join(", ")}. The correct answers are: ${correctAnswers}.`);
+
+  //display each answer along with the correct answer using a template literal
+  //"When asked [question] you answered:  . The correct answer is:   ."
+
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
